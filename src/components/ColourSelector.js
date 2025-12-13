46ea106fbb82f26a14539900b2ad16960f8202c2
Himanshu Singh <himanshusingh1k@gmail.com>
2025-12-13 14:18:16 +0530

    commit

--- src/components/ColourSelector.js
+++ src/components/ColourSelector.js
@@ -2,14 +2,14 @@ import React from "react";
 
 const ColourSelector = (props) => {
   const { config, selectNextBackground } = props;
-  const { background, classname, label } = config;
+  const { background, classname, label, key } = config;
 
   function handleClick() {
     selectNextBackground({ background: background });
   }
 
   return (
-    <button className={classname} onClick={handleClick}>{label}</button>
+    <button className={classname} onClick={handleClick} data-testid={key}>{label}</button>
   ); // prettier-ignore
 };
 
