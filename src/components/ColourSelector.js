0845cdc4783a137f8c789d116aab2e93350cb0b3
Himanshu Singh <himanshusingh1k@gmail.com>
2025-12-13 14:12:51 +0530

    update

--- src/components/ColourSelector.js
+++ src/components/ColourSelector.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React from "react";
 
 const ColourSelector = (props) => {
-  const { config, selectNextBackground } = props
-  const { background } = config;
+  const { config, selectNextBackground } = props;
+  const { background, classname, label } = config;
+
+  function handleClick() {
+    selectNextBackground({ background: background });
+  }
+
   return (
-    <button className={/* classname should come here */} onClick={() => selectNextBackground({background: background})}>
-      {/* label should come here */}
-    </button>
-  )
-}
+    <button className={classname} onClick={handleClick}>{label}</button>
+  ); // prettier-ignore
+};
+
 export default ColourSelector;
--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,16 @@
+import React, { useState } from "react";
 
+const Selection = (props) => {
+  const { applyColor } = props;
+
+  function setBackground(evt) {
+    const elem = evt.target;
+    elem.style.backgroundColor = applyColor();
+  }
+
+  return (
+    <div className="fix-box subheading" onClick={setBackground}>Selection</div>
+  ); // prettier-ignore
+};
+
+export default Selection;
